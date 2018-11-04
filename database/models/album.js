9c88ab037db42c0e53b96cd2c725d92f794e79f5
Daniel Varela <dotexe0@gmail.com>
2018-11-04 09:59:07 -0800

    mongoose models for album and artist

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,17 @@
 // Todo: create Album Schema
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const AlbumSchema = new Schema({
+  title: { type: String },
+  date: { type: Date },
+  copiesSold: { type: Number },
+  numberTracks: { type: Array },
+  image: { type: String },
+  revenue: { type: Number }
+});
+
+const Album = mongoose.model('album', AlbumSchema);
+
+module.exports = Album;
--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,21 @@
 // Todo: Create Artist Model
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ArtistSchema = new Schema({
+  name: { type: String },
+  age: { type: Number },
+  yearsActive: { type: Number },
+  image: { type: String },
+  genre: { type: String },
+  website: { type: String },
+  netWorth: { type: Number },
+  labelName: { type: String },
+  retired: { type: Boolean },
+  albumns: [{ type: mongoose.Types.ObjectId, ref: 'albumn' }]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+
+module.exports = Artist;
